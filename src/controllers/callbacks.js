ac2b44c9c7302a1e73d2a89728fc9484459a0186
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-05-19 16:06:02 -0400

    callbacks functions added

---
+++ src/controllers/callbacks.js
@@ -0,0 +1,87 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+// Configura la conexión a la base de datos PostgreSQL a través de variables de entorno
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    allowExitOnIdle: true
+});
+
+
+const getPosts =async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM posts order by id');
+        const posts = result.rows;
+        res.json(posts);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: `Error al obtener los posts ${process.env.DB_USER} ${process.env.DB_HOST} ${process.env.DB_NAME} ${process.env.DB_PORT}` });
+    }
+}
+const postPost =async (req, res) => {
+    const { titulo, url, descripcion } = req.body;
+    const img=url;
+    //console.log(titulo, img, descripcion);
+    const likes = 0;
+    try {
+        const client = await pool.connect();
+        const result = await client.query(
+            'INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING id',
+            [titulo, img, descripcion, likes]
+        );
+        const postId = result.rows[0].id;
+        //console.log(result.rows[0]);
+        res.json({ id: postId, titulo, img, descripcion, likes });
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error al insertar el post' });
+    }
+}
+
+const putPost =async (req, res) => {
+    const id = req.params.id;
+    const client = await pool.connect();
+    let likes=0;
+    try {
+        const result2 = await client.query('SELECT likes FROM posts where id=$1', [id]);
+        const data=result2.rows[0];
+        likes=data.likes+1;
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    try {
+        const result = await client.query('UPDATE posts SET likes = $1 WHERE id = $2 RETURNING *', [likes, id]);
+        const updatedPost = result.rows[0];
+        res.json(updatedPost);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        client.release();
+    }
+}
+
+const deletePosts =async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const client = await pool.connect();
+        await client.query('DELETE FROM posts WHERE id = $1', [id]);
+        client.release();
+        res.sendStatus(204);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+}
+
+module.exports ={getPosts, postPost, putPost, deletePosts};
--- src/index.js
+++ src/index.js
@@ -2,105 +2,42 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 require('dotenv').config();
-
 const cors = require('cors');
+const {getPosts, postPost, putPost, deletePosts} = require('./controllers/callbacks');
 
 const app = express();
 //generar constante que determina el puerto a usar
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 // Configura body-parser para parsear las solicitudes JSON
 app.use(bodyParser.json());
 
-// Configura la conexión a la base de datos PostgreSQL
+// Configura la conexión a la base de datos PostgreSQL a través de variables de entorno
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    allowExitOnIdle: true
 });
 
 
 // Ruta para obtener todos los posts
-app.get('/posts', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM posts order by id');
-        const posts = result.rows;
-        res.json(posts);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: `Error al obtener los posts ${process.env.DB_USER} ${process.env.DB_HOST} ${process.env.DB_NAME} ${process.env.DB_PORT}` });
-    }
-});
-
-//Ruta para aumentar (actualizar) los likes
-app.put('/posts/like/:id', async (req, res) => {
-    const id = req.params.id;
-    const client = await pool.connect();
-    let likes=0;
-    try {
-        const result2 = await client.query('SELECT likes FROM posts where id=$1', [id]);
-        const data=result2.rows[0];
-        likes=data.likes+1;
-    } catch (error) {
-        console.error('Error executing query', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    try {
-        const result = await client.query('UPDATE posts SET likes = $1 WHERE id = $2 RETURNING *', [likes, id]);
-        const updatedPost = result.rows[0];
-        res.json(updatedPost);
-    } catch (error) {
-        console.error('Error executing query', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    } finally {
-        client.release();
-    }
-});
+app.get('/posts',getPosts);
 
 // Ruta para insertar un nuevo post
-app.post('/posts', async (req, res) => {
-    const { titulo, img, descripcion } = req.body;
-    //console.log(titulo, img, descripcion);
-    const likes = 0;
-    try {
-        const client = await pool.connect();
-        const result = await client.query(
-            'INSERT INTO posts (titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4) RETURNING id',
-            [titulo, img, descripcion, likes]
-        );
-        const postId = result.rows[0].id;
-        //console.log(result.rows[0]);
-        res.json({ id: postId, titulo, img, descripcion, likes });
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Error al insertar el post' });
-    }
-});
+app.post('/posts', postPost, putPost);
+
+//Ruta para aumentar (actualizar) los likes
+app.put('/posts/like/:id', putPost);
 
 //Ruta para borrar post
-app.delete('/posts/:id', async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const client = await pool.connect();
-        await client.query('DELETE FROM posts WHERE id = $1', [id]);
-        client.release();
-        res.sendStatus(204);
-    } catch (err) {
-        console.error(err);
-        res.sendStatus(500);
-    }
-});
+app.delete('/posts/:id', deletePosts);
 
 // Inicia el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor de Express en ${process.env.DB_HOST} escuchando en el puerto ${PORT}`);
+    console.log(`Servidor de Express escuchando en el puerto ${PORT}`);
 });
